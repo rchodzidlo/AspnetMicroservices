b10e4da0b2a3824d3cb6dc260000e740ce441d29
Rafał Chodzidło <raflotraflo@gmail.com>
2022-02-25 00:47:24 +0100

    discout controller

---
+++ src/Services/Doscount/Doscount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Doscount.API.Entities;
+using System.Threading.Tasks;
+
+namespace Doscount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
